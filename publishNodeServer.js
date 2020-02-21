60d263651ae63a7d8dd0c7e8b08ff0124582506f
Ren Mingrui <972931182@qq.com>
2020-02-22 00:40:28 +0800

    由于腾讯云暂时不支持从git拉代码，修改为上传zip文件

--- publishNodeServer.js
+++ publishNodeServer.js
@@ -1,5 +1,6 @@
 const tencentcloud = require("tencentcloud-sdk-nodejs");
 const Credential = tencentcloud.common.Credential;
+const fs = require('fs')
 
 // 导入对应产品模块的client models。
 const ScfClient = tencentcloud.scf.v20180416.Client;
@@ -10,17 +11,12 @@ async function publish() {
   const client = new ScfClient(cred, "ap-guangzhou")
   const req = new models.ListFunctionsRequest()
 
-  req.Action = 'UpdateFunctionCode',
-  req.Version =  '2018-04-16',
-  req.Region =  'ap-guangzhou',
-  req.Handler =  'serverlessEntry.handle',
-  req.FunctionName =  'everything-house-fe',
-  req.CodeSource =  'Git',
-  req.Code =  {
-    GitUrl: 'https://github.com/jiladahe1997/everything-house-fe.git',
-    GitDirectory: "serverDist",
-    GitBranch: 'dev'
-  }
+  req.Action = 'UpdateFunctionCode'
+  req.Version =  '2018-04-16'
+  req.Region =  'ap-guangzhou'
+  req.Handler =  'serverlessEntry.handle'
+  req.FunctionName =  'everything-house-fe'
+  req.ZipFile = fs.readFileSync('./index.zip').toString('base64')
 
   return await new Promise((resolve, reject)=> {
     client.UpdateFunctionCode(req, (err, res)=>{
